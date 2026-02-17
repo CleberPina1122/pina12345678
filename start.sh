d3a98e74a75827c7babc0f4a63094814669f1eb4
CleberPina1122 <cb.play618@gmail.com>
2026-02-17 16:52:11 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# Este script inicializa o servidor Uvicorn para FastAPI
 
+# Verifica se o script está sendo executado como root e não faz nada se estiver
+if [ "$(id -u)" = "0" ]; then
+    echo "Não execute como root!" 1>&2
+    exit 1
+fi
+
+# Inicializando o servidor Uvicorn
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
